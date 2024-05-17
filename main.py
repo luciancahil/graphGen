ef716607bab1075ca8ff79d341c638432c582a71
luciancahil <royhe62@yahoo.ca>
2024-05-17 12:08:45 -0700

    Changed Main

--- main.py
+++ main.py
@@ -1,143 +1,135 @@
-from train import *
+
+import argparse
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import os
+from random import shuffle
+import torch
+import torch.nn as nn
+import torch.nn.init as init 
+from torch.autograd import Variable 
+import torch.nn.functional as F
+from torch import optim
+from torch.optim.lr_scheduler import MultiStepLR
+
+import data
+from baselines.graphvae.model import GraphVAE
+from baselines.graphvae.data import GraphAdjSampler
+
+CUDA = 2
 
 device = torch.device("cpu")  # or "cuda" if you have a GPU
 
-if __name__ == '__main__':
-    # All necessary arguments are defined in args.py
-    args = Args()
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(args.cuda)
-    print('CUDA', args.cuda)
-    print('File name prefix',args.fname)
-    # check if necessary directories exist
-    if not os.path.isdir(args.model_save_path):
-        os.makedirs(args.model_save_path)
-    if not os.path.isdir(args.graph_save_path):
-        os.makedirs(args.graph_save_path)
-    if not os.path.isdir(args.figure_save_path):
-        os.makedirs(args.figure_save_path)
-    if not os.path.isdir(args.timing_save_path):
-        os.makedirs(args.timing_save_path)
-    if not os.path.isdir(args.figure_prediction_save_path):
-        os.makedirs(args.figure_prediction_save_path)
-    if not os.path.isdir(args.nll_save_path):
-        os.makedirs(args.nll_save_path)
 
-    time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    # logging.basicConfig(filename='logs/train' + time + '.log', level=logging.DEBUG)
-    if args.clean_tensorboard:
-        if os.path.isdir("tensorboard"):
-            shutil.rmtree("tensorboard")
-    configure("tensorboard/run"+time, flush_secs=5)
+LR_milestones = [500, 1000]
 
-    graphs = create_graphs.create(args)
-    
-    # split datasets
-    random.seed(123)
-    shuffle(graphs)
-    graphs_len = len(graphs)
-    graphs_test = graphs[int(0.8 * graphs_len):]
-    graphs_train = graphs[0:int(0.8*graphs_len)]
-    graphs_validate = graphs[0:int(0.2*graphs_len)]
+def build_model(args, max_num_nodes):
+    out_dim = max_num_nodes * (max_num_nodes + 1) // 2
+    if args.feature_type == 'id':
+        input_dim = max_num_nodes
+    elif args.feature_type == 'deg':
+        input_dim = 1
+    elif args.feature_type == 'struct':
+        input_dim = 2
+    model = GraphVAE(input_dim, 64, 256, max_num_nodes)
+    return model
 
-    # if use pre-saved graphs
-    # dir_input = "/dfs/scratch0/jiaxuany0/graphs/"
-    # fname_test = dir_input + args.note + '_' + args.graph_type + '_' + str(args.num_layers) + '_' + str(
-    #     args.hidden_size_rnn) + '_test_' + str(0) + '.dat'
-    # graphs = load_graph_list(fname_test, is_real=True)
-    # graphs_test = graphs[int(0.8 * graphs_len):]
-    # graphs_train = graphs[0:int(0.8 * graphs_len)]
-    # graphs_validate = graphs[int(0.2 * graphs_len):int(0.4 * graphs_len)]
+def train(args, dataloader, model):
+    epoch = 1
+    optimizer = optim.Adam(list(model.parameters()), lr=args.lr)
+    scheduler = MultiStepLR(optimizer, milestones=LR_milestones, gamma=args.lr)
 
+    model.train()
+    for epoch in range(5000):
+        for batch_idx, data in enumerate(dataloader):
+            model.zero_grad()
+            features = data['features'].float()
+            adj_input = data['adj'].float()
 
-    graph_validate_len = 0
-    for graph in graphs_validate:
-        graph_validate_len += graph.number_of_nodes()
-    graph_validate_len /= len(graphs_validate)
-    print('graph_validate_len', graph_validate_len)
+            features = Variable(features).to(device)
+            adj_input = Variable(adj_input).to(device)
+            
+            loss = model(features, adj_input)
+            print('Epoch: ', epoch, ', Iter: ', batch_idx, ', Loss: ', loss)
+            loss.backward()
 
-    graph_test_len = 0
-    for graph in graphs_test:
-        graph_test_len += graph.number_of_nodes()
-    graph_test_len /= len(graphs_test)
-    print('graph_test_len', graph_test_len)
+            optimizer.step()
+            scheduler.step()
+            break
 
+def arg_parse():
+    parser = argparse.ArgumentParser(description='GraphVAE arguments.')
+    io_parser = parser.add_mutually_exclusive_group(required=False)
+    io_parser.add_argument('--dataset', dest='dataset', 
+            help='Input dataset.')
 
+    parser.add_argument('--lr', dest='lr', type=float,
+            help='Learning rate.')
+    parser.add_argument('--batch_size', dest='batch_size', type=int,
+            help='Batch size.')
+    parser.add_argument('--num_workers', dest='num_workers', type=int,
+            help='Number of workers to load data.')
+    parser.add_argument('--max_num_nodes', dest='max_num_nodes', type=int,
+            help='Predefined maximum number of nodes in train/test graphs. -1 if determined by \
+                  training data.')
+    parser.add_argument('--feature', dest='feature_type',
+            help='Feature used for encoder. Can be: id, deg')
 
-    args.max_num_node = max([graphs[i].number_of_nodes() for i in range(len(graphs))])
-    max_num_edge = max([graphs[i].number_of_edges() for i in range(len(graphs))])
-    min_num_edge = min([graphs[i].number_of_edges() for i in range(len(graphs))])
+    parser.set_defaults(dataset='grid',
+                        feature_type='id',
+                        lr=0.001,
+                        batch_size=1,
+                        num_workers=1,
+                        max_num_nodes=-1)
+    return parser.parse_args()
 
-    # args.max_num_node = 2000
-    # show graphs statistics
-    print('total graph num: {}, training set: {}'.format(len(graphs),len(graphs_train)))
-    print('max number node: {}'.format(args.max_num_node))
-    print('max/min number edge: {}; {}'.format(max_num_edge,min_num_edge))
-    print('max previous node: {}'.format(args.max_prev_node))
+def main():
+    prog_args = arg_parse()
 
-    # save ground truth graphs
-    ## To get train and test set, after loading you need to manually slice
-    save_graph_list(graphs, args.graph_save_path + args.fname_train + '0.dat')
-    save_graph_list(graphs, args.graph_save_path + args.fname_test + '0.dat')
-    print('train and test graphs saved at: ', args.graph_save_path + args.fname_test + '0.dat')
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(CUDA)
+    print('CUDA', CUDA)
+    ### running log
 
-    ### comment when normal training, for graph completion only
-    # p = 0.5
-    # for graph in graphs_train:
-    #     for node in list(graph.nodes()):
-    #         # print('node',node)
-    #         if np.random.rand()>p:
-    #             graph.remove_node(node)
-        # for edge in list(graph.edges()):
-        #     # print('edge',edge)
-        #     if np.random.rand()>p:
-        #         graph.remove_edge(edge[0],edge[1])
+    if prog_args.dataset == 'enzymes':
+        graphs= data.Graph_load_batch(min_num_nodes=10, name='ENZYMES')
+        num_graphs_raw = len(graphs)
+    elif prog_args.dataset == 'grid':
+        graphs = []
+        for i in range(2,3):
+            for j in range(2,3):
+                graphs.append(nx.grid_2d_graph(i,j))
+        num_graphs_raw = len(graphs)
 
-
-    ### dataset initialization
-    if 'nobfs' in args.note:
-        print('nobfs')
-        dataset = Graph_sequence_sampler_pytorch_nobfs(graphs_train, max_num_node=args.max_num_node)
-        args.max_prev_node = args.max_num_node-1
-    if 'barabasi_noise' in args.graph_type:
-        print('barabasi_noise')
-        dataset = Graph_sequence_sampler_pytorch_canonical(graphs_train,max_prev_node=args.max_prev_node)
-        args.max_prev_node = args.max_num_node - 1
+    if prog_args.max_num_nodes == -1:
+        max_num_nodes = max([graphs[i].number_of_nodes() for i in range(len(graphs))])
     else:
-        dataset = Graph_sequence_sampler_pytorch(graphs_train,max_prev_node=args.max_prev_node,max_num_node=args.max_num_node)
-    sample_strategy = torch.utils.data.sampler.WeightedRandomSampler([1.0 / len(dataset) for i in range(len(dataset))],
-                                                                     num_samples=args.batch_size*args.batch_ratio, replacement=True)
-    dataset_loader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, num_workers=args.num_workers,
-                                               sampler=sample_strategy)
+        max_num_nodes = prog_args.max_num_nodes
+        # remove graphs with number of nodes greater than max_num_nodes
+        graphs = [g for g in graphs if g.number_of_nodes() <= max_num_nodes]
 
-    ### model initialization
-    ## Graph RNN VAE model
-    # lstm = LSTM_plain(input_size=args.max_prev_node, embedding_size=args.embedding_size_lstm,
-    #                   hidden_size=args.hidden_size, num_layers=args.num_layers).to(device)
+    graphs_len = len(graphs)
+    print('Number of graphs removed due to upper-limit of number of nodes: ', 
+            num_graphs_raw - graphs_len)
+    graphs_test = graphs[int(0.8 * graphs_len):]
+    #graphs_train = graphs[0:int(0.8*graphs_len)]
+    graphs_train = graphs
 
-    if 'GraphRNN_VAE_conditional' in args.note:
-        rnn = GRU_plain(input_size=args.max_prev_node, embedding_size=args.embedding_size_rnn,
-                        hidden_size=args.hidden_size_rnn, num_layers=args.num_layers, has_input=True,
-                        has_output=False).to(device)
-        output = MLP_VAE_conditional_plain(h_size=args.hidden_size_rnn, embedding_size=args.embedding_size_output, y_size=args.max_prev_node).to(device)
-    elif 'GraphRNN_MLP' in args.note:
-        rnn = GRU_plain(input_size=args.max_prev_node, embedding_size=args.embedding_size_rnn,
-                        hidden_size=args.hidden_size_rnn, num_layers=args.num_layers, has_input=True,
-                        has_output=False).to(device)
-        output = MLP_plain(h_size=args.hidden_size_rnn, embedding_size=args.embedding_size_output, y_size=args.max_prev_node).to(device)
-    elif 'GraphRNN_RNN' in args.note:
-        rnn = GRU_plain(input_size=args.max_prev_node, embedding_size=args.embedding_size_rnn,
-                        hidden_size=args.hidden_size_rnn, num_layers=args.num_layers, has_input=True,
-                        has_output=True, output_size=args.hidden_size_rnn_output).to(device)
-        output = GRU_plain(input_size=1, embedding_size=args.embedding_size_rnn_output,
-                           hidden_size=args.hidden_size_rnn_output, num_layers=args.num_layers, has_input=True,
-                           has_output=True, output_size=1).to(device)
+    print('total graph num: {}, training set: {}'.format(len(graphs),len(graphs_train)))
+    print('max number node: {}'.format(max_num_nodes))
 
-    ### start training
-    train(args, dataset_loader, rnn, output)
+    dataset = GraphAdjSampler(graphs_train, max_num_nodes, features=prog_args.feature_type)
+    #sample_strategy = torch.utils.data.sampler.WeightedRandomSampler(
+    #        [1.0 / len(dataset) for i in range(len(dataset))],
+    #        num_samples=prog_args.batch_size, 
+    #        replacement=False)
+    dataset_loader = torch.utils.data.DataLoader(
+            dataset, 
+            batch_size=prog_args.batch_size, 
+            num_workers=prog_args.num_workers)
+    model = build_model(prog_args, max_num_nodes).to(device)
+    train(prog_args, dataset_loader, model)
 
-    ### graph completion
-    # train_graph_completion(args,dataset_loader,rnn,output)
 
-    ### nll evaluation
-    # train_nll(args, dataset_loader, dataset_loader, rnn, output, max_iter = 200, graph_validate_len=graph_validate_len,graph_test_len=graph_test_len)
-
+if __name__ == '__main__':
+    main()
